f49bb0713a840b03fb757488142e3dd373dc25ff
Jorge Alvarez Sone <j.alvarezsone@gmail.com>
2021-08-02 11:15:06 +0200

    created imagenes fn

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const { series, src, dest, watch } = require('gulp');
 const gulpSass = require('gulp-sass');
 const dartSass = require('dart-sass');
+const imagemin = require('gulp-imagemin');
+const notify = require('gulp-notify');
 
 const sass = gulpSass(dartSass);
 
@@ -11,7 +13,20 @@ const paths = {
 };
 
 function css() {
-  return src(paths.scss).pipe(sass()).pipe(dest('./build/css'));
+  return src(paths.scss)
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      }),
+    )
+    .pipe(dest('./build/css'));
+}
+
+function imagenes() {
+  return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest('./build/img'))
+    .pipe(notify({ message: 'Imagen Minificada' }));
 }
 
 function watchArchivos() {
@@ -20,6 +35,7 @@ function watchArchivos() {
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
 
-exports.default = series(css, watchArchivos);
+exports.default = series(css, imagenes, watchArchivos);
